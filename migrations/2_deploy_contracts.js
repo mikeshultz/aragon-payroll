fdd3252c79efecd09b5126980566eedf3d9ad541
Mike Shultz <shultzm@gmail.com>
2017-07-20 16:23:42 -0600

    Shuffled around dependencies for deploy and renamed from PayrollInterface to Payroll

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,15 @@
 var HumanStandardToken = artifacts.require("HumanStandardToken");
 var USDToken = artifacts.require("USDToken");
-var PayrollInterface = artifacts.require("PayrollInterface");
+var Payroll = artifacts.require("Payroll");
 
 /* It appears I can not test multiple tokens of the same contract using truffle.
    https://github.com/trufflesuite/truffle/issues/237
 
    So... There's a separate duplicate contract with the name USDToken
  */
-module.exports = function(deployer) {
-  deployer.deploy(HumanStandardToken, 60000000, "BestToken", 18, "BT");
-  deployer.deploy(USDToken, 6000000000, "United States Dollar", 2, "USD");
-  deployer.deploy(PayrollInterface);
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(HumanStandardToken, 10000000000000, "Human Standard Token", 18, "HT");
+  deployer.deploy(USDToken, 100000000, "United States Dollar", 2, "USD").then(function() {
+    return deployer.deploy(Payroll, accounts[0], accounts[1], USDToken.address);
+  });
 };
